aed3944915d2088166ecfe4538ae9b83bf0ff2cd
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-02-06 01:40:10 -0800

    Color for menu items

--- src/js/znode/ui.js
+++ src/js/znode/ui.js
@@ -15,15 +15,21 @@ $(function(){
      }
   });
 
+  var current_menu = "#eraser";
+
   // ui code
   var openWin = $("#openWin");
   openWin.hide();
 
   $(".btn").mouseenter(function(){
     $(this).animate({"backgroundColor" : "white"}, 200);
-  }).mouseleave(function(){
-    $(this).animate({"backgroundColor" : "#efefef"});
+  }).mouseleave(function() {
+    if ($(this).hasClass("active"))
+      $(this).animate({"backgroundColor": "yellow"});
+    else
+      $(this).animate({"backgroundColor" : "#efefef"});
   });
+
   $("#clear").click(function(){
     graph.clearAll();
   });
@@ -78,57 +84,76 @@ $(function(){
     graph.clear_and_render();
   });
 
+  function make_menu_active(menu_item) {
+    $(current_menu).removeClass("active");
+    $(current_menu).css({"background-color": '#efefef'});
+    $(menu_item).addClass("active");
+    $(menu_item).css({"background-color": 'yellow'});
+    current_menu = menu_item;
+  }
+
   $("#eraser").click(function() {
+    make_menu_active("#eraser");
     graph.set_current_tool("eraser");
     graph.draw_menu_changed();
   });
 
   $("#shaky_line").click(function() {
+    make_menu_active("#shaky_line");
     graph.set_current_tool("shaky_line");
     graph.draw_menu_changed();
   });
 
   $("#straight_line").click(function() {
+    make_menu_active("#straight_line");
     graph.set_current_tool("straight_line");
     graph.draw_menu_changed();
   });
 
   $("#free_hand_line").click(function() {
+    make_menu_active("#free_hand_line");
     graph.set_current_tool("free_hand_line");
     graph.draw_menu_changed();
   });
 
   $("#arrow").click(function() {
+    make_menu_active("#arrow");
     graph.set_current_tool("arrow");
     graph.draw_menu_changed();
   });
 
   $("#circle").click(function() {
+    make_menu_active("#circle");
     graph.set_current_tool("circle");
     graph.draw_menu_changed();
   });
 
   $("#ellipse").click(function() {
+    make_menu_active("#ellipse");
     graph.set_current_tool("ellipse");
     graph.draw_menu_changed();
   });
 
   $("#shaky_rect").click(function() {
+    make_menu_active("#shaky_rect");
     graph.set_current_tool("shaky_rect");
     graph.draw_menu_changed();
   });
 
   $("#rect").click(function() {
+    make_menu_active("#rect");
     graph.set_current_tool("rect");
     graph.draw_menu_changed();
   });
 
   $("#text").click(function() {
+    make_menu_active("#text");
     graph.set_current_tool("text");
     graph.draw_menu_changed();
   });
 
   $("#select").click(function() {
+    make_menu_active("#select");
     graph.set_current_tool("select_mode");
     graph.draw_menu_changed();
   });
